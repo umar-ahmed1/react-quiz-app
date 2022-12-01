983b8a46acaf3b9f6e88534747ea835fe13def1d
Umar Ahmed <mian.ahmed@ucalgary.ca>
2022-12-01 16:21:46 -0700

    display the answers in random order and as buttons

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
 export default function Question(props){
-    console.log(props.correct_answer)
+    //create an array with all the answers then shuffle it
+    const allAnswers = [props.correct_answer,...props.incorrect_answers]
+    for (var i = allAnswers.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = allAnswers[i];
+        allAnswers[i] = allAnswers[j];
+        allAnswers[j] = temp;
+    }
+    console.log(allAnswers)
+
+    const answerButtonElements = allAnswers.map(answer => {
+        return <button className="answer-button">{answer}</button>
+    })
+
+
     return(
         <div className="trivia-question">
             <div className="trivia-question-title">{props.question}</div>
+            <div className="answers-div">{answerButtonElements}</div>
         </div>
 
     )
