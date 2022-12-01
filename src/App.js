de9419960a2fce51c8cf768a726ea401fcfd06df
Umar Ahmed <mian.ahmed@ucalgary.ca>
2022-12-01 02:23:17 -0700

    deleted data.js

--- src/App.js
+++ src/App.js
@@ -37,3 +37,19 @@ export default function App() {
     </main>
   );
 }
+
+
+/*
+    //make the api call and store the data
+    let GENERAL_KNOWLEDGE = fetch('https://opentdb.com/api.php?amount=5&category=9')
+    .then(res => res.json())
+    .then(dataRecieved => dataRecieved.results)
+
+    let SCIENCE_NATURE = fetch('https://opentdb.com/api.php?amount=5&category=17')
+    .then(res => res.json())
+    .then(dataRecieved => dataRecieved.results)
+
+    let ANIMALS = fetch('https://opentdb.com/api.php?amount=5&category=27')
+    .then(res => res.json())
+    .then(dataRecieved => ANIMALS = dataRecieved.results)
+    */
