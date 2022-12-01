dc8542e830d73471bd5e6bfc08fc669a8dc12c5a
Umar Ahmed <mian.ahmed@ucalgary.ca>
2022-11-30 22:19:33 -0700

    added api call to get the trivia questions

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,6 +1,18 @@
 import React from 'react'
 
 export default function Select(){
+    //make the api call and store the data as a state
+    const [allQuestions,setAllQuestions] = React.useState([])
+    React.useEffect( () => {
+        fetch('https://opentdb.com/api.php?amount=10')
+        .then(res => res.json())
+        .then(dataRecieved => console.log(dataRecieved))
+    },[]);    
+
+
+
+
+
     return (
         <div className="select-screen">
           <div>Developed by Umar Ahmed</div>
