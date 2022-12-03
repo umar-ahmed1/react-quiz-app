1e407e67c36be076a839fdc9d864e0e3ad343f58
Umar Ahmed <mian.ahmed@ucalgary.ca>
2022-12-02 18:14:38 -0700

    fixed some css

--- src/components/Question.js
+++ src/components/Question.js
@@ -5,7 +5,7 @@ export default function Question(props){
 
     function chooseStyle(answer){
         if(answer === props.selected_answer){
-            return {backgroundColor:"D6DBF6",
+            return {backgroundColor:"D6DBF5",
                     border: "2px solid #4D5B9E"}
         } else{
             return {backgroundColor:"white"}
